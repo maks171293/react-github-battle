f44c2b30c66233e178c6b894a0b53cfe81d9c956
Maks <andreev171293@gmail.com>
2017-05-11 20:06:40 +0300

    Add navigation menu and routing

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Popular from './Popular';
+import ReactRouter from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 class App extends React.Component {
   render(){
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route render={()=>{
+              return <div>Sorry, address not Found</div>
+            }}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
